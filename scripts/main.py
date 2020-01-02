8c0138aeed196d7b9e5adf69985a9c0c05681dc2
phuc <phucduong2397@gmail.com>
2020-01-02 13:11:42 +0700

    segmentation

---
+++ scripts/main.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python
+
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import CompressedImage
+import rospy
+import cv2
+import numpy as np
+# from lane_detect import pipeline
+from PIL import Image
+from segment_function import load_model_segmentation, segment_image
+import numpy as np
+import cv2
+
+net = 0
+COLORS = 0
+count = 0
+count_image = 0
+def callback_depth(data):
+	print("depth")
+	bridge = CvBridge()
+	try:
+		np_arr = np.fromstring(data.data, np.uint8)
+		cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+	except CvBridgeError as e:
+		print(e)
+
+	cv2.imshow('depth', cv_image)
+	cv2.waitKey(3)
+
+
+def callback_rgb(data):
+	global count, count_image
+	global net, COLORS
+	print("rgb")
+	bridge = CvBridge()
+	try:
+		np_arr = np.fromstring(data.data, np.uint8)
+		cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+	except CvBridgeError as e:
+		print(e)
+
+	cv2.imshow('rgb', cv_image)
+
+	#write image
+	if count % 20 == 0:
+		#write real image
+		cv2.imwrite('data/'+'realImage/'+str(count_image)+'.jpg', np.array(cv_image))
+		print("save image" + 'data/'+'realImage/'+str(count_image)+'.jpg')
+
+		#show segmentation image
+		pathRealImage = "/home/phucxo/" + 'data/' + 'realImage/' + str(count_image) + '.jpg'
+		inputSegmentImage = cv2.imread(pathRealImage)
+		segmentImage = segment_image(inputSegmentImage, net, COLORS)
+
+		cv2.imwrite('data/' + 'segmentImage/' + str(count_image) + '.jpg', np.array(segmentImage))
+		cv2.imshow("segmentImage", segmentImage)
+		cv2.waitKey(3)
+
+		count_image += 1
+
+	count += 1
+
+
+
+
+
+if __name__ == '__main__':
+	global net, COLORS
+	rospy.init_node('video_stream', anonymous=True)
+	print('run')
+	net, COLORS = load_model_segmentation()
+	# image_sub_depth = rospy.Subscriber('team1/camera/depth/compressed', CompressedImage, callback_depth)
+	image_sub_rgb = rospy.Subscriber('team1/camera/rgb/compressed', CompressedImage, callback_rgb)
+	rospy.spin()
---
+++ scripts/segment_function.py
@@ -0,0 +1,67 @@
+# USAGE
+# python segment.py --image images/10.jpg
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+
+def load_model_segmentation():
+    CLASSES = open('/home/phucxo/catkin_ws/src/get_image/scripts/enet-cityscapes/enet-classes.txt').read().strip().split("\n")
+    COLORS = open('/home/phucxo/catkin_ws/src/get_image/scripts/enet-cityscapes/enet-color-black.txt').read().strip().split("\n")
+    COLORS = [np.array(c.split(",")).astype("int") for c in COLORS]
+    COLORS = np.array(COLORS, dtype="uint8")
+
+    legend = np.zeros(((len(CLASSES) * 25) + 25, 300, 3), dtype="uint8")
+
+    for (i, (className, color)) in enumerate(zip(CLASSES, COLORS)):
+        color = [int(c) for c in color]
+        cv2.putText(legend, className, (5, (i * 25) + 17),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.rectangle(legend, (100, (i * 25)), (300, (i * 25) + 25),
+                      tuple(color), -1)
+
+    print("[INFO] loading model...")
+    net = cv2.dnn.readNet('/home/phucxo/catkin_ws/src/get_image/scripts/enet-cityscapes/enet-model.net')
+    print("load model segment success")
+    return net, COLORS
+
+
+def segment_image(input_image, net, COLORS):
+    image = imutils.resize(input_image, width=320)
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (1024, 512), 0,
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    output = net.forward()
+    end = time.time()
+
+    print("[INFO] inference took {:.4f} seconds".format(end - start))
+    (numClasses, height, width) = output.shape[1:4]
+    print("(numClasses, height, width) = output.shape[1:4]")
+    print(output.shape[1:4])
+    classMap = np.argmax(output[0], axis=0)
+    print("output[0]")
+    print(output[0])
+    mask = COLORS[classMap]
+
+    mask = cv2.resize(mask, (image.shape[1], image.shape[0]),
+                      interpolation=cv2.INTER_NEAREST)
+
+    output = (1 * mask).astype("uint8")
+
+    return output
+
+
+
+# net, COLORS = load_model_segmentation()
+#
+# image = cv2.imread('images/10.jpg')
+# output = segment_image(image, net, COLORS)
+#
+# # cv2.imshow("Legend", legend)
+# cv2.imshow("Input", image)
+# cv2.imshow("Output", output)
+# cv2.waitKey(0)
